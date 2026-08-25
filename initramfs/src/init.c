397fae0b435aebbe4f2f07b2e4d2d0228ebbdf0f
maverick <jiabo2011@gmail.com>
2026-08-25 09:03:23 +0800

    add a new simple init for mini-os

--- initramfs/src/init.c
+++ initramfs/src/init.c
@@ -1,7 +1,29 @@
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main(void)
 {
-    write(1, "hello\n", 6);
-    return 0;
+    char *argv[] = {
+        "busybox",
+        "echo",
+        "hello from busybox",
+        NULL
+    };
+    char *envp[] = {
+        NULL
+    };
+    pid_t pid;
+
+    pid = fork();
+    if (pid == 0) {
+        execve("/bin/busybox", argv, envp);
+        write(2, "exec busybox failed\n", 20);
+        _exit(1);
+    }
+
+    if (pid > 0)
+        waitpid(pid, NULL, 0);
+
+    for (;;)
+        ;
 }
