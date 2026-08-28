8ad3ef15622d2a7fe3ae28bc1600e8871079849b
maverick <jiabo2011@gmail.com>
2026-08-28 14:57:25 +0800

    run busybox ash as PID 2

--- initramfs/src/init.c
+++ initramfs/src/init.c
@@ -1,48 +1,38 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-static int run_busybox(char *const argv[])
-{
-    pid_t pid;
-    int status;
-
-    pid = fork();
-    if (pid < 0) {
-        write(2, "fork failed\n", 12);
-        return 127;
-    }
-    if (pid == 0) {
-        execve("/bin/busybox", argv, (char *[]){ NULL });
-        write(2, "exec busybox failed\n", 20);
-        _exit(127);
-    }
-
-    if (waitpid(pid, &status, 0) < 0)
-        return 127;
-    if (WIFEXITED(status))
-        return WEXITSTATUS(status);
-    return 127;
-}
-
-static void echo_status(const char *label, int st)
-{
-    char c = '0' + (char)(st % 10);
-    unsigned long n = 0;
-
-    while (label[n])
-        n++;
-    write(1, label, n);
-    write(1, &c, 1);
-    write(1, "\n", 1);
-}
+static char *shell_argv[] = { "ash", "-l", NULL };
+static char *shell_envp[] = {
+    "PATH=/bin",
+    "HOME=/",
+    "TERM=linux",
+    NULL,
+};
 
 int main(void)
 {
-    int st;
-    char *ps_argv[] = { "busybox", "ps", NULL };
+    for (;;) {
+        pid_t pid;
+        int status;
 
-    st = run_busybox(ps_argv);
-    echo_status("ps $?=", st);
+        pid = fork();
+        if (pid < 0) {
+            write(2, "fork failed\n", 12);
+            return 1;
+        }
+
+        if (pid == 0) {
+            execve("/bin/busybox", shell_argv, shell_envp);
+            write(2, "exec ash failed\n", 16);
+            _exit(127);
+        }
+
+        if (waitpid(pid, &status, 0) == pid) {
+            write(2, "ash exited, restarting\n", 23);
+            if (WIFEXITED(status) && WEXITSTATUS(status) == 0)
+                break;
+        }
+    }
 
     for (;;)
         ;
